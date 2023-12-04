e22d187a9a1a858edba33a36f88ff02a1b2f5be9
hsitaram <hariswaran@gmail.com>
2023-12-04 09:53:38 -0700

    making parallel plate compatible to new chemistry interface

--- test/parallelPlateDC/Chemistry.cpp
+++ test/parallelPlateDC/Chemistry.cpp
@@ -4,10 +4,35 @@
 namespace plasmachem
 {
     amrex::Vector<std::string> specnames(NUM_SPECIES);
+    AMREX_GPU_DEVICE_MANAGED amrex::Real rct_rxnarray[NUM_REACTIONS][NUM_ALL_SPECIES]={0.0};
+    AMREX_GPU_DEVICE_MANAGED amrex::Real pdt_rxnarray[NUM_REACTIONS][NUM_ALL_SPECIES]={0.0};
+    AMREX_GPU_DEVICE_MANAGED amrex::Real reaction_elecenergy[NUM_REACTIONS]={0.0};
+    AMREX_GPU_DEVICE_MANAGED amrex::Real reaction_gasenergy[NUM_REACTIONS]={0.0};
 
     void init()
     {
         specnames[I_ID]="Ion";
+        
+        for(int i=0;i<NUM_REACTIONS;i++)
+        {
+            for(int j=0;j<NUM_ALL_SPECIES;j++)
+            {
+                rct_rxnarray[i][j]=0.0;
+                pdt_rxnarray[i][j]=0.0;
+            }
+        }
+
+        int rnum=0;
+        
+        //E + N --> I + 2E
+        rct_rxnarray[rnum][EDN_ID] = 1.0;
+        rct_rxnarray[rnum][N_ID]   = 1.0;
+
+        pdt_rxnarray[rnum][I_ID]   = 1.0;
+        pdt_rxnarray[rnum][EDN_ID] = 2.0;
+        
+        reaction_elecenergy[rnum]  = -24.0*ECHARGE;
+        reaction_gasenergy[rnum]   = 0.0;
     }    
     void close()
     {
@@ -30,6 +55,9 @@ namespace plasmachem
         amrex::Real charge=0.0;
         switch(specid)
         {
+            case N_ID:
+                charge=0.0;
+                break;               
             case I_ID:
                 charge=1.0;
                 break;               
@@ -48,6 +76,9 @@ namespace plasmachem
         amrex::Real molwt=M_AMU;
         switch(specid)
         {
+            case N_ID:
+                molwt=4.0*M_AMU;
+                break;               
             case I_ID:
                 molwt=4.0*M_AMU;
                 break;               
@@ -59,4 +90,14 @@ namespace plasmachem
         }
         return(molwt);
     }
+    
+    AMREX_GPU_HOST_DEVICE void get_reaction_rateconstants(amrex::Real Te,amrex::Real Tg, amrex::Real Pg,
+                                                   amrex::Real efield, amrex::Real spec[NUM_ALL_SPECIES],
+                                                   amrex::Real rateconst[NUM_REACTIONS])
+   {
+        int rnum=0;
+        //E + N --> I + 2E
+        rateconst[rnum] = 2.5e-12
+            *std::exp(-278508.0/Te);
+   }
 }
